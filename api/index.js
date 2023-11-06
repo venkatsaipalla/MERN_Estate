a7db82118f52a547ffae760d5c95e3a11f2ec6e4
venkatsaipalla <venkatasaipalla2019@email.com>
2023-11-06 17:11:14 +0530

    coonection to db

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("Mongodb connected successfully"))
+  .catch((error) => console.log(error));
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
-
