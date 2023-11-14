c840bb10f9439ced1ca246789e5c3ef144eff58e
venkatsaipalla <venkatasaipalla2019@email.com>
2023-11-14 13:47:56 +0530

    added title and logo

---
+++ client/src/pages/CreateListing.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function CreateListing() {
+  return (
+    <main>
+      <h1 className="text-3xl font-semibold text-center my-7">
+        Create a Listing
+      </h1>
+      <form>
+        
+      </form>
+    </main>
+  );
+}
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -7,6 +7,7 @@ import {
   getDownloadURL,
 } from "firebase/storage";
 import { app } from "../firebase";
+import { Link } from "react-router-dom";
 import {
   updateUserStart,
   updateUserSuccess,
@@ -190,6 +191,10 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "update"}
         </button>
+        <Link
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+          to={"/create-listing"}
+        >Create Listing</Link>
       </form>
 
       <div className="flex justify-between mt-5">
