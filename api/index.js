1869ef5a1c677aa50f177b66a587be4197453961
venkatsaipalla <venkatasaipalla2019@email.com>
2023-11-18 18:56:18 +0530

    spaces fix

--- api/index.js
+++ api/index.js
@@ -32,6 +32,7 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
