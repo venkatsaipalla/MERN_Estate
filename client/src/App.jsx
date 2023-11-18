ff9b8e0b6ac3f35d720293578faaf533963ccbd1
venkatsaipalla <venkatasaipalla2019@email.com>
2023-11-18 17:01:09 +0530

    complete header search form functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,8 @@ import CreateListing from "./pages/CreateListing";
 import PrivateRoute from "./components/PrivateRoute";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
+import Search from './pages/Search';
+
 export default function App() {
   return (
     <BrowserRouter>
@@ -18,12 +20,13 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
+        <Route path='/search' element={<Search />} />
         <Route path="/listing/:listingId" element={<Listing />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
           <Route
-            path='/update-listing/:listingId'
+            path="/update-listing/:listingId"
             element={<UpdateListing />}
           />
         </Route>
